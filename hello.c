a405c8b463beda30eb8420bf62484b1ca2c13b15
Syamir <syamirmdin@gmail.com>
2021-04-10 17:26:15 +0800

    Revert "First time commit"
    This reverts commit 6c66d242dc30584283c971f85ea60699b78820f6.

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-/* This is my first program in C */
-printf("Hello World!");
-printf("I Love C\n");
-return (0);
-}
--- helloworld.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(void) {
-/* This is my first program in C */
-printf(“Hello World!”);
-printf(“I Love C”);
-return (0);
-}
